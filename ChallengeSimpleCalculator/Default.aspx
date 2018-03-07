ee04e5eadaad2ad1c01091e4f48314768640aba1
Yohei <yo_kubocchi@hotmail.com>
2018-03-07 02:42:07 -0800

    Fixed

---
+++ ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Simple Calculator</h1>
+            <br />
+            First Value:
+            <asp:TextBox ID="firstValueTextBox" runat="server"></asp:TextBox>
+            <br />
+            Second Value:
+            <asp:TextBox ID="secondValueTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="additionButton" runat="server" OnClick="additionButton_Click" Text="+" Width="30px" />
+&nbsp;
+            <asp:Button ID="subtractionButton" runat="server" OnClick="subtractionButton_Click" Text="-" Width="30px" />
+&nbsp;
+            <asp:Button ID="timesButton" runat="server" OnClick="timesButton_Click" Text="*" Width="30px" />
+&nbsp;
+            <asp:Button ID="divisionButton" runat="server" OnClick="divisionButton_Click" Text="/" Width="30px" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" style="font-weight: 700; font-size: x-large; background-color: #3399FF"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            string i = firstValueTextBox.Text;
+            string j = secondValueTextBox.Text;
+            double k = double.Parse(i);  // integer firstValue
+            double l = double.Parse(j);  // integer secondValue
+
+            double result = (double)k + (double)l;
+
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            string i = firstValueTextBox.Text;
+            string j = secondValueTextBox.Text;
+            double k = double.Parse(i);  // integer firstValue
+            double l = double.Parse(j);  // integer secondValue
+
+            double result = (double)k - (double)l;
+
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void timesButton_Click(object sender, EventArgs e)
+        {
+            string i = firstValueTextBox.Text;
+            string j = secondValueTextBox.Text;
+            double k = double.Parse(i);  // integer firstValue
+            double l = double.Parse(j);  // integer secondValue
+
+            double result = (double)k * (double)l;
+
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            string i = firstValueTextBox.Text;
+            string j = secondValueTextBox.Text;
+            double k = double.Parse(i);  // integer firstValue
+            double l = double.Parse(j);  // integer secondValue
+
+            double result = (double)k / (double)l;
+
+            resultLabel.Text = result.ToString();
+
+        }
+    }
+}
